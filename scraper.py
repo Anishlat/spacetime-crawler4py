1794aa51ca2249c37ff19bb6779910b447d2f516
Bavhead <guptabhavyaji@gmail.com>
2023-04-23 21:51:11 -0700

    use pathlib for saving pages/links/etc, completed extract_next_links

--- scraper.py
+++ scraper.py
@@ -1,50 +1,61 @@
 import re
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup as bs
+from pathlib import Path
 
 # Takes a URL and a response object and returns a list of filtered links from the response object.
-def scraper(url, resp):
+def scraper(url, resp, save_to_disk=False, save_to_folder='scraped_pages'):
     links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    filtered_links = [link for link in links if is_valid(link)]
+
+    # Save content of current page to local file if save_to_disk is True
+    if save_to_disk:
+        save_web_page(url, resp, save_to_folder)
+        
+    return filtered_links
+
+def save_web_page(url, resp, save_to_folder):
+    folder_path = Path(save_to_folder)
 
 
+# Implementation required.
+# url: the URL that was used to get the page
+# resp.url: the actual url of the page
+# resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+# resp.error: when status is not 200, you can check the error here, if needed.
+# resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+#         resp.raw_response.url: the url, again
+#         resp.raw_response.content: the content of the page!
+# Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
 def extract_next_links(url, resp):
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-
     # If status code != 200, error, return empty list
     if (resp.status != 200):
         return []
 
     soupified = bs(resp.raw_response.content)   # BeautifulSoup object
     aTags = soupified.select('a')               # list of all <a> tags
-    hyperlinks = [link['href'] for link in aTags if link['href'] != url and link['href'] != resp.url]
+    hyperlinks = {link['href'].partition("#")[0] for link in aTags if link['href'] != url and link['href'] != resp.url}  # set of all hyperlinks without fragment, if any; exclude self-referential & duplicate links
 
-    return hyperlinks
+    return list(hyperlinks)
 
 
-
+# Decide whether to crawl this url or not. 
+# If you decide to crawl it, return True; otherwise return False.
+# There are already some conditions that return False.
 def is_valid(url):
-    # Decide whether to crawl this url or not. 
-    # If you decide to crawl it, return True; otherwise return False.
-    # There are already some conditions that return False.
     try:
         parsed = urlparse(url)
 
+        #invalid scheme
         if parsed.scheme not in set(["http", "https"]):
             return False
 
+        #invalid hostname
         accepted_hostnames = {"ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu"}   
-        if not any([parsed.hostname.endswith(hostname) for hostname in accepted_hostnames]):   # invalid hostname
+        if not any([parsed.hostname.endswith(hostname) for hostname in accepted_hostnames]):
             return False
 
+        # invalid file extension
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
