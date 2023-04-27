17a3eb403b2a8052d10aeae5d85272fabf93d946
Bavhead <guptabhavyaji@gmail.com>
2023-04-27 12:26:36 -0700

    added creation of data storing files

--- launch.py
+++ launch.py
@@ -5,13 +5,15 @@ from utils.server_registration import get_cache_server
 from utils.config import Config
 from crawler import Crawler
 
+from utils.baseFileCreation import create_base_files 
 
-def main(config_file, restart): #, save_to_disk):
+
+def main(config_file, restart):
     cparser = ConfigParser()
     cparser.read(config_file)
     config = Config(cparser)
     config.cache_server = get_cache_server(config, restart)
-    crawler = Crawler(config, restart) #, save_to_disk)
+    crawler = Crawler(config, restart)
     crawler.start()
 
 
@@ -21,4 +23,6 @@ if __name__ == "__main__":
     parser.add_argument("--config_file", type=str, default="config.ini")
     # parser.add_argument("--save_to_disk", action="store_true", default=False)
     args = parser.parse_args()
+    create_base_files()
+
     main(args.config_file, args.restart)
